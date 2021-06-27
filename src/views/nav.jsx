15a05e10a7651caf59250e522c93258b5b171fc7
namhla1221 <namhlamthi21@gmail.com>
2021-06-27 22:56:48 +0200

    fixing the error

--- src/views/nav.jsx
+++ src/views/nav.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
+
 
 export default class Nav extends Component {
   handleLogout = () => {
