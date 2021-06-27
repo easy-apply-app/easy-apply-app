442c6816307f504601f56f1af20715bfe66068cd
namhla1221 <namhlamthi21@gmail.com>
2021-06-27 23:05:06 +0200

    new changes

--- src/views/nav.jsx
+++ src/views/nav.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import Button from "@material-ui/core/Button";
 
 export default class Nav extends Component {
   handleLogout = () => {
@@ -25,28 +25,39 @@ export default class Nav extends Component {
       buttons = (
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link to={"/login"} onClick={this.handleLogout} className="nav-link">
+            <Link
+              to={"/login"}
+              onClick={this.handleLogout}
+              className="nav-link"
+            >
               Login
             </Link>
           </li>
           <li className="nav-item">
-            <Link to={"/register"} onClick={this.handleLogout} className="nav-link">
+            <Link
+              to={"/register"}
+              onClick={this.handleLogout}
+              className="nav-link"
+            >
               Sign Up
             </Link>
           </li>
         </ul>
       );
+      
     }
     return (
-      <nav className="navbar navbar-expand navbar-light fixed-top">
-        <div>
-         
-          <Button variant="contained" href="/">Home</Button>
-          <Button variant="contained" href="/register">Sign Up</Button>
-          <Button variant="contained" href="/login">Login</Button>
-          
-        </div>
-      </nav>
+      <div>
+        <Button variant="contained" href="/">
+          Home
+        </Button>
+        <Button variant="contained" href="/register">
+          Sign Up
+        </Button>
+        <Button variant="contained" href="/login">
+          Login
+        </Button>
+      </div>
     );
   }
 }
